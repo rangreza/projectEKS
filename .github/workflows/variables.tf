0b93ebf1338db0e8fc6546abdabb5492e7409342
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-15 01:16:19 +0530

    Create variables.tf
    var

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "eks-cluster"
+}
