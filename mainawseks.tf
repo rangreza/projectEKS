6aca80b14c2684e648f3711aec199a8b87fe244c
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-15 01:33:24 +0530

    Update mainawseks.tf

--- mainawseks.tf
+++ mainawseks.tf
@@ -13,17 +13,10 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 
+  enable_nat_gateway   = true          # Enables NAT Gateway
+  single_nat_gateway   = true          # Use one NAT Gateway for all private subnets
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  # Enable NAT Gateway and Elastic IP
-  enable_nat_gateway   = true
-  single_nat_gateway   = true   # Use a single NAT Gateway for cost savings
-  one_nat_gateway_per_az = false
-
-  # Create Elastic IP for NAT Gateway
-  create_nat_gateway   = true
-  create_eip           = true
 
   tags = {
     Name = "my-vpc"
@@ -35,7 +28,7 @@ module "eks" {
   version = "20.13.0"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.30"
+  cluster_version = "1.27"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = concat(module.vpc.public_subnets, module.vpc.private_subnets)
 
@@ -64,4 +57,19 @@ resource "aws_security_group_rule" "allow_ssh_eks" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = module.eks.cluster_security_group_id
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "EKS cluster security group ID"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "nat_gateway_eip" {
+  description = "Elastic IP associated with the NAT Gateway"
+  value       = module.vpc.nat_public_ip
 }
