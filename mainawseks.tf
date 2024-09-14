197cfd9f05f44bf21c844ab52101d62bcd8e6ff3
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-15 01:25:31 +0530

    Create mainawseks.tf
    main file with eks 1.30

---
+++ mainawseks.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  # Enable NAT Gateway and Elastic IP
+  enable_nat_gateway   = true
+  single_nat_gateway   = true   # Use a single NAT Gateway for cost savings
+  one_nat_gateway_per_az = false
+
+  # Create Elastic IP for NAT Gateway
+  create_nat_gateway   = true
+  create_eip           = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.13.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.30"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+
+  enable_irsa = true
+
+  tags = {
+    Name = "eks-cluster"
+  }
+}
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = module.vpc.default_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_ssh_eks" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = module.eks.cluster_security_group_id
+}
